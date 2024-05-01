93597439c5e58b50bdcfb081e7f5e84fdbeb96b9
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2024-05-01 22:45:57 +0300

    fix

--- practicaltest/src/main/java/com/example/practicaltest/model/User.java
+++ practicaltest/src/main/java/com/example/practicaltest/model/User.java
@@ -4,17 +4,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 @Builder
 public class User {
     @NotBlank
