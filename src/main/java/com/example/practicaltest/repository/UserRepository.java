f225f38cb2f05db8a44abf53f68160899d294c15
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2024-05-05 01:17:00 +0300

    add getter

--- src/main/java/com/example/practicaltest/repository/UserRepository.java
+++ src/main/java/com/example/practicaltest/repository/UserRepository.java
@@ -2,38 +2,36 @@ package com.example.practicaltest.repository;
 
 import com.example.practicaltest.dto.request.PatchUserRequestDto;
 import com.example.practicaltest.model.User;
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
-@RequiredArgsConstructor
+@Repository
+@Getter
 public class UserRepository {
+
     private final List<User> users = new ArrayList<>();
 
-    public User save(final User user) {
+    public User save(User user) {
         if (users.stream()
                 .anyMatch(x -> x.getEmail().equals(user.getEmail()))) {
             throw new ResponseStatusException(HttpStatus.CONFLICT);
         } else
             users.add(user);
-        ResponseEntity.status(HttpStatus.CREATED);
-        return user;
+        return findByEmail(user.getEmail());
     }
 
     public String delete(String email) {
         if (users.stream()
                 .anyMatch(x -> x.getEmail().equals(email))) {
             users.removeIf(x -> x.getEmail().equals(email));
-            ResponseEntity.status(HttpStatus.ACCEPTED);
+            return null;
         } else throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        return null;
     }
 
     public List<User> findByBirthDate(LocalDate fromDate, LocalDate toDate) {
@@ -52,38 +50,34 @@ public class UserRepository {
     }
 
     public User update(User user, String email) {
-        int indexUserFound = users.indexOf(findByEmail(email));
+        int indexFoundUser = users.indexOf(findByEmail(email));
         user.setEmail(email); // you can't change your email
-        users.set(indexUserFound, user);
-        return users.get(indexUserFound); // ResponseEntity.status(HttpStatus.ACCEPTED).body(userFound);
+        users.set(indexFoundUser, user);
+        return users.get(indexFoundUser);
     }
 
     public User patch(PatchUserRequestDto patchUser, String email) {
-        User userFound = findByEmail(email);
-        String userEmail;
+        User foundUser = findByEmail(email);
         String firstName;
         String lastName;
         LocalDate birthDate;
         String address;
         String phoneNumber;
-        if ((userEmail = patchUser.getEmail()) != null) {
-            userFound.setEmail(userEmail);
-        }
         if ((firstName = patchUser.getFirstName()) != null) {
-            userFound.setFirstName(firstName);
+            foundUser.setFirstName(firstName);
         }
         if ((lastName = patchUser.getLastName()) != null) {
-            userFound.setLastName(lastName);
+            foundUser.setLastName(lastName);
         }
         if ((birthDate = patchUser.getBirthDate()) != null) {
-            userFound.setBirthDate(birthDate);
+            foundUser.setBirthDate(birthDate);
         }
         if ((address = patchUser.getAddress()) != null) {
-            userFound.setAddress(address);
+            foundUser.setAddress(address);
         }
         if ((phoneNumber = patchUser.getPhoneNumber()) != null) {
-            userFound.setPhoneNumber(phoneNumber);
+            foundUser.setPhoneNumber(phoneNumber);
         }
-        return userFound;
+        return foundUser;
     }
 }
