9921dc643a5f3d188c72c26ae19f9cf5de9b497f
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2024-05-05 23:06:41 +0300

    fix

--- src/main/java/com/example/practicaltest/dto/request/PatchUserRequestDto.java
+++ src/main/java/com/example/practicaltest/dto/request/PatchUserRequestDto.java
@@ -1,38 +1,25 @@
 package com.example.practicaltest.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Value;
 
 import java.time.LocalDate;
 
 @Value
+@Builder
 @AllArgsConstructor
 public class PatchUserRequestDto {
 
-    @NotBlank
-    @Email(regexp = "^(?=.{10,63}$)(?!.*\\s)"
-            + "[a-zA-Z0-9!#$%&'*+\\-=?^_`{|}~]+"
-            + "(?:[.\\-][a-zA-Z0-9!#$%&'*+\\-=?^_`{|}~]+)*"
-            + "@[a-z0-9](?:[a-z0-9\\-]*[a-z0-9])*"
-            + "(?:\\.[a-z0-9](?:[a-z0-9\\-]*[a-z0-9])*)+",
-            message = "Email should be valid") String email;
-
-    @NotBlank
     String firstName;
-    @NotBlank
+
     String lastName;
 
     @Past(message = "Birth date should be valid")
-    @NotNull
     LocalDate birthDate;
 
-    @NotBlank
     String address;
 
-    @NotBlank
     String phoneNumber;
 }
