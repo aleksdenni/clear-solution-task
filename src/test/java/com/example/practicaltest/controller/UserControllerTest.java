074d283ebf36bd8c99b5991f37fd27d572fdc1ad
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2024-05-06 13:49:59 +0300

    fix tests

--- src/test/java/com/example/practicaltest/controller/UserControllerTest.java
+++ src/test/java/com/example/practicaltest/controller/UserControllerTest.java
@@ -18,12 +18,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -36,6 +39,8 @@ class UserControllerTest {
     private MockMvc mockMvc;
     @MockBean
     private UserService userService;
+
+    private String baseUri;
     private User user;
     private UserRequestDto userRequest;
     private UserResponseDto userResponse;
@@ -43,6 +48,9 @@ class UserControllerTest {
 
     @BeforeEach
     public void setUp() {
+
+        baseUri = "/api/v1/users";
+
         user = User.builder()
                 .email("test@email.com")
                 .firstName("testName")
@@ -82,19 +90,11 @@ class UserControllerTest {
     @Test
     void testCreateUser() throws Exception {
 
-        String userRequestJson = String.format(
-                "{\"email\":\"%s\"," +
-                        " \"firstName\":\"%s\"," +
-                        " \"lastName\":\"%s\"," +
-                        " \"birthDate\":\"%s\"}",
-                userRequest.getEmail(),
-                userRequest.getFirstName(),
-                userRequest.getLastName(),
-                userRequest.getBirthDate().toString());
+        String userRequestJson = objectMapper.writeValueAsString(userRequest);
 
         when(userService.createUser(any(UserRequestDto.class)))
-                .thenReturn(ResponseEntity.status(HttpStatus.CREATED).body(userResponse));
-        mockMvc.perform(post("/api/v1/users")
+                .thenReturn(userResponse);
+        mockMvc.perform(post(baseUri)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(userRequestJson))
                 .andExpect(status().isCreated())
@@ -105,18 +105,51 @@ class UserControllerTest {
     }
 
     @Test
+    void getUsers() throws Exception {
+        List<User> foundUsers = new ArrayList<>(List.of(user));
+        when(userService.getUsers(any(LocalDate.class), any(LocalDate.class))).thenReturn(foundUsers);
+
+        mockMvc.perform(get(baseUri + "/search")
+                        .param("fromBirthDate", "1999-01-01")
+                        .param("toBirthDate", "2001-01-01"))
+                .andExpect(status().isAccepted())
+                .andExpect(jsonPath("$.length()",equalTo(1)))
+                .andExpect(jsonPath("$[0].email").value("test@email.com"));
+    }
+
+    @Test
+    void testPatchUser() throws Exception{
+        userResponse = objectMapper.updateValue(userResponse, patchUserRequestDto);
+
+        when(userService.patchUser(anyString(), any(PatchUserRequestDto.class))).thenReturn(userResponse);
+        
+        String patchRequestJson = objectMapper.writeValueAsString(patchUserRequestDto);
+
+        mockMvc.perform(patch(baseUri + "/test@email.com")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(patchRequestJson))
+                .andExpect(status().isAccepted())
+                .andExpect(jsonPath("$.firstName").value("patchedTestName"));
+
+    }
+
+    @Test
     void testUpdateUser() throws Exception {
-        String userRequestJson;
-        userRequestJson = objectMapper.writeValueAsString(userRequest);
+        String userRequestJson = objectMapper.writeValueAsString(userRequest);
 
-        when(userService.updateUser(any(String.class), any(UserRequestDto.class)))
-                .thenReturn(ResponseEntity.status(HttpStatus.ACCEPTED).body(userResponse));
+        when(userService.updateUser(anyString(), any(UserRequestDto.class)))
+                .thenReturn(userResponse);
 
-        mockMvc.perform(put("/api/v1/users/test@email.com")
+        mockMvc.perform(put(baseUri + "/test@email.com")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(userRequestJson))
                 .andExpect(status().isAccepted())
                 .andExpect(jsonPath("$.firstName", equalTo(userRequest.getFirstName())));
     }
 
+    @Test
+    void deleteUser() throws Exception {
+        mockMvc.perform(delete(baseUri + "/test@email.com"))
+                .andExpect(status().isNoContent());
+    }
 }
