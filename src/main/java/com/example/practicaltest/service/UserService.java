e7fabe07237770a075ea35473ab7bdd397ffdfd0
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2024-05-06 18:13:58 +0300

    add method for check age and improve update and patch

--- src/main/java/com/example/practicaltest/service/UserService.java
+++ src/main/java/com/example/practicaltest/service/UserService.java
@@ -18,34 +18,44 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
     @Value("${min.user.age}")
     private String minUserAge;
     private final UserMapper userMapper;
     private final UserRepository userRepository;
 
     public UserResponseDto createUser(UserRequestDto userRequest) {
-        if (LocalDate.now().minusYears(Integer.parseInt(minUserAge)).isBefore(userRequest.getBirthDate())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
+        checkAgeOfMajority(userRequest.getBirthDate());
         final User user = userMapper.toModel(userRequest);
         final User savedUser = userRepository.save(user);
         return userMapper.toResponseDto(savedUser);
     }
 
     public List<User> getUsers(LocalDate fromDate, LocalDate toDate) {
-        List<User> foundUsers = userRepository.findByBirthDate(fromDate, toDate);
-        if (foundUsers.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        } else return foundUsers;
+        return userRepository.findByBirthDate(fromDate, toDate);
     }
 
+    /**
+     * @param email               email of the user being updated
+     * @param patchUserRequestDto new data of the user without email
+     *                            and with changeable fields for patch
+     * @return patched user
+     */
     public UserResponseDto patchUser(String email, PatchUserRequestDto patchUserRequestDto) {
-        final User savedUser = userRepository.patch(email, patchUserRequestDto);
+        if (patchUserRequestDto.getBirthDate() != null) {
+            checkAgeOfMajority(patchUserRequestDto.getBirthDate());
+        }
+        final User user = userMapper.toModel(patchUserRequestDto);
+        final User savedUser = userRepository.patch(email, user);
         return userMapper.toResponseDto(savedUser);
     }
 
+    /**
+     * @param email       email of the user being updated
+     * @param userRequest new data of the user with all fields for update
+     * @return updated user
+     */
     public UserResponseDto updateUser(String email, UserRequestDto userRequest) {
+        checkAgeOfMajority(userRequest.getBirthDate());
         final User user = userMapper.toModel(userRequest);
         final User savedUser = userRepository.update(email, user);
         return userMapper.toResponseDto(savedUser);
@@ -53,5 +63,16 @@ public class UserService {
 
     public void deleteUser(String email) {
         userRepository.delete(email);
+    }
+
+    public void checkAgeOfMajority(LocalDate birthDate) {
+        if (birthDate == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Must be valid date");
+        }
+        if (LocalDate.now().minusYears(Integer.parseInt(minUserAge)).isBefore(birthDate)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Must be " + minUserAge + " years or older");
+        }
     }
 }
