1a50b01a6cb0502c8b4c3d8141e90a2827d97b72
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2024-05-06 13:49:16 +0300

    move up ResponseEntity to the controller

--- src/main/java/com/example/practicaltest/controller/UserController.java
+++ src/main/java/com/example/practicaltest/controller/UserController.java
@@ -7,9 +7,11 @@ import com.example.practicaltest.model.User;
 import com.example.practicaltest.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -24,27 +26,40 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserResponseDto> createUser(@RequestBody @Valid UserRequestDto userRequest) {
-        return userService.createUser(userRequest);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.createUser(userRequest));
     }
 
     @GetMapping("/search")
     public ResponseEntity<List<User>> getUsers(@RequestParam LocalDate fromBirthDate,
                                @RequestParam LocalDate toBirthDate) {
-        return userService.getUsers(fromBirthDate, toBirthDate);
+        if (fromBirthDate.isAfter(toBirthDate)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(userService.getUsers(fromBirthDate, toBirthDate));
     }
 
     @PatchMapping("/{email}")
     public ResponseEntity<UserResponseDto> patchUser(@PathVariable String email, @Valid @RequestBody PatchUserRequestDto patchUserRequestDto){
-        return userService.patchUser(email, patchUserRequestDto);
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(userService.patchUser(email, patchUserRequestDto));
     }
 
     @PutMapping("/{email}")
     public ResponseEntity<UserResponseDto> updateUser(@PathVariable String email, @Valid @RequestBody UserRequestDto userRequestDto) {
-        return userService.updateUser(email, userRequestDto);
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(userService.updateUser(email, userRequestDto));
     }
 
     @DeleteMapping("/{email}")
-    public void deleteUser(@PathVariable String email) {
-        userService.deleteUser(email);
+    public ResponseEntity<Void> deleteUser(@PathVariable String email) {
+        try {
+            userService.deleteUser(email);
+            return ResponseEntity.noContent().build();
+        } catch (ResponseStatusException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
+
 }
